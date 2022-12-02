cd7beeb609726c4ae267fbc4c59974c48560f28e
Max Crone <code@maxcrone.org>
2022-12-02 09:06:50 +0100

    Add day 2 part 1

---
+++ src/bin/02.rs
@@ -0,0 +1,91 @@
+use std::str::FromStr;
+
+use anyhow::{anyhow, Error, Result};
+use itertools::Itertools;
+
+#[derive(Clone, Debug, PartialEq)]
+enum Outcome {
+    Win = 6,
+    Lose = 0,
+    Draw = 3,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Move {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+trait Beats {
+    fn beats(&self) -> Self;
+}
+
+impl Beats for Move {
+    fn beats(&self) -> Self {
+        match *self {
+            Move::Rock => Move::Scissors,
+            Move::Paper => Move::Rock,
+            Move::Scissors => Move::Paper,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Round {
+    opponent: Move,
+    me: Move,
+}
+
+impl FromStr for Round {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (o, m) = s
+            .split_once(" ")
+            .unwrap();
+
+        let opponent = match o {
+            "A" => Move::Rock,
+            "B" => Move::Paper,
+            "C" => Move::Scissors,
+            _ => return Err(anyhow!("Can't parse opponent's move")),
+        };
+        let me = match m {
+            "X" => Move::Rock,
+            "Y" => Move::Paper,
+            "Z" => Move::Scissors,
+            _ => return Err(anyhow!("Can't parse our move")),
+        };
+
+        Ok(Round { opponent, me })
+    }
+}
+
+impl Round {
+    fn outcome(&self) -> Outcome {
+        if self.me.beats() == self.opponent {
+            Outcome::Win
+        } else if self.me == self.opponent {
+            Outcome::Draw
+        } else {
+            Outcome::Lose
+        }
+    }
+    fn score(&self) -> usize {
+        self.me as usize + self.outcome() as usize
+    }
+}
+
+#[aoc::main(02)]
+fn main(input: &str) -> (usize, usize) {
+    let strategy: Vec<Round> = input.lines()
+        .map(|s| s.parse().unwrap())
+        .collect_vec();
+
+    let p1 = strategy.iter()
+        .map(|r| r.score())
+        .sum();
+    
+    (p1, 0)
+}
