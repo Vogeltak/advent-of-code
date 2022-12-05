756f8c7a3b83e91937230a25da74181a86b5e13f
Max Crone <code@maxcrone.org>
2022-12-05 14:01:22 +0100

    Refactor day 2
    Based on a blog post by fasterthanlime:
    https://fasterthanli.me/series/advent-of-code-2022/part-2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
+use std::convert::TryFrom;
 
-use anyhow::{anyhow, Error, Result};
+use color_eyre::Report;
 use itertools::Itertools;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -10,6 +11,29 @@ enum Outcome {
     Draw = 3,
 }
 
+impl TryFrom<&str> for Outcome {
+    type Error = Report;
+
+    fn try_from(c: &str) -> Result<Self, Self::Error> {
+        match c {
+            "X" => Ok(Self::Lose),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Win),
+            _ => Err(color_eyre::eyre::eyre!("not a valid outcome: {c:?}")),
+        }
+    }
+}
+
+impl Outcome {
+    fn matching_move(self, other: Move) -> Move {
+        match self {
+            Outcome::Win => other.winning_move(),
+            Outcome::Draw => other.drawing_move(),
+            Outcome::Lose => other.losing_move(),
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Move {
     Rock = 1,
@@ -17,12 +41,22 @@ enum Move {
     Scissors = 3,
 }
 
-trait Beats {
-    fn beats(&self) -> Self;
-    fn beaten_by(&self) -> Self;
+impl TryFrom<&str> for Move {
+    type Error = Report;
+
+    fn try_from(c: &str) -> Result<Self, Self::Error> {
+        match c {
+            "A" => Ok(Move::Rock),
+            "B" => Ok(Move::Paper),
+            "C" => Ok(Move::Scissors),
+            _ => Err(color_eyre::eyre::eyre!("not a valid move: {c:?}")),
+        }
+    }
 }
 
-impl Beats for Move {
+impl Move {
+    const ALL_MOVES: [Move; 3] = [Move::Rock, Move::Paper, Move::Scissors];
+
     fn beats(&self) -> Self {
         match *self {
             Move::Rock => Move::Scissors,
@@ -31,43 +65,46 @@ impl Beats for Move {
         }
     }
 
-    fn beaten_by(&self) -> Self {
-        match *self {
-            Move::Rock => Move::Paper,
-            Move::Paper => Move::Scissors,
-            Move::Scissors => Move::Rock,
-        }
+    fn winning_move(self) -> Self {
+        Self::ALL_MOVES
+            .iter()
+            .copied()
+            .find(|m| m.beats() == self)
+            .expect("at least one move beats me")
+    }
+
+    fn losing_move(self) -> Self {
+        Self::ALL_MOVES
+            .iter()
+            .copied()
+            .find(|&m| self.beats() == m)
+            .expect("I beat at least one move")
+    }
+
+    fn drawing_move(self) -> Self {
+        self
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Round {
     opponent: Move,
     me: Move,
 }
 
 impl FromStr for Round {
-    type Err = Error;
+    type Err = Report;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (o, m) = s
             .split_once(' ')
             .unwrap();
 
-        let opponent = match o {
-            "A" => Move::Rock,
-            "B" => Move::Paper,
-            "C" => Move::Scissors,
-            _ => return Err(anyhow!("Can't parse opponent's move")),
-        };
-        let me = match m {
-            "X" => opponent.beats(),
-            "Y" => opponent,
-            "Z" => opponent.beaten_by(),
-            _ => return Err(anyhow!("Can't parse intended outcome")),
-        };
+        let opponent = Move::try_from(o)?;
+        let outcome = Outcome::try_from(m)?;
+        let me = outcome.matching_move(opponent);
 
-        Ok(Round { opponent, me })
+        Ok(Self { opponent, me })
     }
 }
 
@@ -88,6 +125,8 @@ impl Round {
 
 #[aoc::main(02)]
 fn main(input: &str) -> (usize, usize) {
+    color_eyre::install().unwrap();
+
     let strategy: Vec<Round> = input.lines()
         .map(|s| s.parse().unwrap())
         .collect_vec();
