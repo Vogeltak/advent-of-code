ca2ad5154d3dc180cc772ef8f9759c524aae4599
Max Crone <code@maxcrone.org>
2022-12-07 10:24:31 +0100

    Add day 7 part 1

---
+++ src/bin/07.rs
@@ -0,0 +1,45 @@
+use std::path::PathBuf;
+
+use hashbrown::HashMap;
+use itertools::Itertools;
+
+#[aoc::main(07)]
+fn main(input: &str) -> (i64, i64) {
+    let mut wd = PathBuf::new();
+    let mut dirs: HashMap<PathBuf, i64> = HashMap::new();
+
+    for l in input.lines() {
+        let cmd = l.trim().split_ascii_whitespace().collect_vec();
+        match cmd.len() {
+            3 => {
+                let dir = cmd[2];
+                if dir == ".." {
+                    wd.pop();
+                } else {
+                    wd.push(dir);
+                }
+            },
+            2 => {
+                match (cmd[0], cmd[1]) {
+                    ("$", "ls") => continue,
+                    ("dir", _) => continue,
+                    (size, _) => {
+                        let size = size.parse::<i64>().unwrap();
+                        for dir in wd.as_path().ancestors() {
+                            dirs.entry(dir.to_path_buf().clone())
+                                .and_modify(|n| *n += size)
+                                .or_insert(size);
+                        }
+                    }
+                }
+            },
+            _ => unreachable!(),
+        }
+    }
+
+    let p1 = dirs.values()
+        .filter(|&&size| size <= 100_000)
+        .sum();
+
+    (p1, 0)
+}
