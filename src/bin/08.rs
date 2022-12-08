7abf01e58c42020e338ef8d8f49deaeca4710eb8
Max Crone <code@maxcrone.org>
2022-12-08 09:50:20 +0100

    Add day 8 part 2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,8 +1,11 @@
+use std::cmp::max;
+
 use itertools::Itertools;
 
 #[aoc::main(08)]
 fn main(input: &str) -> (usize, usize) {
-    let grid = input.lines()
+    let grid = input
+        .lines()
         .map(|l| l.as_bytes().iter().map(|b| b - b'0').collect_vec())
         .collect_vec();
 
@@ -10,14 +13,40 @@ fn main(input: &str) -> (usize, usize) {
 
     for (i_row, row) in grid.iter().enumerate() {
         for (i_col, tree) in row.iter().enumerate() {
-            if grid[i_row][0..i_col].iter().all(|u| u < tree) ||
-                grid[i_row][i_col+1..].iter().all(|u| u < tree) ||
-                grid[0..i_row].iter().all(|u| u[i_col] < *tree) ||
-                grid[i_row+1..].iter().all(|u| u[i_col] < *tree) {
+            if grid[i_row][0..i_col].iter().all(|u| u < tree)
+                || grid[i_row][i_col + 1..].iter().all(|u| u < tree)
+                || grid[0..i_row].iter().all(|u| u[i_col] < *tree)
+                || grid[i_row + 1..].iter().all(|u| u[i_col] < *tree)
+            {
                 p1 += 1;
             }
         }
     }
 
-    (p1, 0)
+    let p2 = (0..grid.len())
+        .cartesian_product(0..grid[0].len())
+        .fold(0, |score, (i, j)| {
+            let tree = grid[i][j];
+            let mut candidate = 1;
+
+            for (di, dj) in [(-1, 0), (0, -1), (1, 0), (0, 1)] {
+                let (mut i, mut j, mut dir_score) = (i, j, 0);
+                while let Some(&next) = grid
+                    .get((i as isize + di) as usize)
+                    .and_then(|x| x.get((j as isize + dj) as usize))
+                {
+                    dir_score += 1;
+                    if tree <= next {
+                        break;
+                    }
+                    i = (i as isize + di) as usize;
+                    j = (j as isize + dj) as usize;
+                }
+                candidate *= dir_score;
+            }
+
+            max(score, candidate)
+        });
+
+    (p1, p2)
 }
