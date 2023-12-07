0e0c2125c3ec53447235b2fc5f2c6d2fc448866d
Max Crone <code@maxcrone.org>
2023-12-07 23:36:47 +0100

    Add day 7 part 2
    Extend with Joker card and logic

--- 2023/src/bin/07.rs
+++ 2023/src/bin/07.rs
@@ -1,9 +1,11 @@
-use std::{cmp::Ordering, collections::HashMap, str::FromStr};
+use std::{cmp::Ordering, collections::HashMap};
 
 use anyhow::Result;
+use itertools::Itertools;
 
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum Card {
+    Joker = 0,
     One = 1,
     Two = 2,
     Three = 3,
@@ -20,10 +22,8 @@ enum Card {
     Ace = 14,
 }
 
-impl TryFrom<char> for Card {
-    type Error = anyhow::Error;
-
-    fn try_from(s: char) -> Result<Self> {
+impl Card {
+    fn new(s: char, p2: bool) -> Result<Self> {
         match s {
             '1' => Ok(Self::One),
             '2' => Ok(Self::Two),
@@ -35,7 +35,10 @@ impl TryFrom<char> for Card {
             '8' => Ok(Self::Eight),
             '9' => Ok(Self::Nine),
             'T' => Ok(Self::Ten),
-            'J' => Ok(Self::Jack),
+            'J' => Ok(match p2 {
+                false => Self::Jack,
+                true => Self::Joker,
+            }),
             'Q' => Ok(Self::Queen),
             'K' => Ok(Self::King),
             'A' => Ok(Self::Ace),
@@ -78,23 +81,43 @@ impl Hand {
         for c in self.cards.clone() {
             card_counts.entry(c).and_modify(|n| *n += 1).or_insert(1);
         }
+        let joker_count = card_counts.get(&Card::Joker).cloned().unwrap_or(0);
 
         match card_counts.keys().len() {
             1 => HandType::Yahtzee,
             2 => {
                 if card_counts.values().any(|n| *n == 4) {
-                    return HandType::FoaK;
+                    return match joker_count > 0 {
+                        true => HandType::Yahtzee,
+                        false => HandType::FoaK,
+                    };
                 }
-                HandType::FullHouse
+                match joker_count {
+                    2 | 3 => HandType::Yahtzee,
+                    _ => HandType::FullHouse,
+                }
             }
             3 => {
                 if card_counts.values().any(|n| *n == 3) {
-                    return HandType::ToaK;
+                    return match joker_count {
+                        1 | 3 => HandType::FoaK,
+                        _ => HandType::ToaK,
+                    };
                 }
-                HandType::TwoPair
+                match joker_count {
+                    2 => HandType::FoaK,
+                    1 => HandType::FullHouse,
+                    _ => HandType::TwoPair,
+                }
             }
-            4 => HandType::OnePair,
-            _ => HandType::HighCard,
+            4 => match joker_count {
+                1 | 2 => HandType::ToaK,
+                _ => HandType::OnePair,
+            },
+            _ => match joker_count {
+                1 => HandType::OnePair,
+                _ => HandType::HighCard,
+            },
         }
     }
 }
@@ -110,16 +133,14 @@ enum HandType {
     Yahtzee,
 }
 
-impl FromStr for Hand {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Hand {
+    fn new(s: &str, p2: bool) -> Result<Self> {
         let (cards, bid) = s
             .split_once(' ')
             .ok_or(anyhow::anyhow!("failed to split line"))?;
         let cards = cards
             .chars()
-            .map(Card::try_from)
+            .map(|c| Card::new(c, p2))
             .filter_map(Result::ok)
             .collect::<Vec<_>>();
         let bid = bid.parse()?;
@@ -130,18 +151,24 @@ impl FromStr for Hand {
 
 #[aoc::main(07)]
 fn main(input: &str) -> (usize, usize) {
-    let mut hands = input
-        .lines()
-        .map(Hand::from_str)
-        .filter_map(Result::ok)
-        .collect::<Vec<_>>();
-
-    hands.sort_unstable();
-
-    let p1 = hands
+    let (p1, p2) = [false, true]
         .iter()
-        .enumerate()
-        .fold(0, |acc, (i, hand)| acc + (i + 1) * hand.bid);
+        .map(|p2| {
+            let mut hands = input
+                .lines()
+                .map(|l| Hand::new(l, *p2))
+                .filter_map(Result::ok)
+                .collect::<Vec<_>>();
 
-    (p1, 0)
+            hands.sort_unstable();
+
+            hands
+                .iter()
+                .enumerate()
+                .fold(0, |acc, (i, hand)| acc + (i + 1) * hand.bid)
+        })
+        .collect_tuple()
+        .unwrap();
+
+    (p1, p2)
 }
