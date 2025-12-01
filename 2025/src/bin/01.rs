e69aae4023112e1a9a246730bccd990c70b22277
Max Crone <code@maxcrone.org>
2025-12-01 11:47:47 +0100

    Add day 1 part 2

--- 2025/src/bin/01.rs
+++ 2025/src/bin/01.rs
@@ -8,23 +8,34 @@ impl Dial {
         Self(50)
     }
 
-    fn turn(&mut self, dir: Direction, clicks: i16) -> AtZero {
+    fn turn(&mut self, dir: Direction, clicks: i16) -> (AtZero, usize) {
         match dir {
             Direction::Left => self.turn_left(clicks),
             Direction::Right => self.turn_right(clicks),
         }
     }
 
-    fn turn_left(&mut self, d: i16) -> AtZero {
+    fn turn_left(&mut self, d: i16) -> (AtZero, usize) {
+        let was_zero = self.0 == 0;
         self.0 -= d;
+        let mut zero_count = (self.0 / 100).abs();
+        // Account for the fact that we're passing zero when going left
+        // when that isn't counted for by the division operator.
+        // With the exception of cases where we *started* from zero.
+        if self.0 <= 0 && !was_zero {
+            zero_count += 1;
+        }
         self.0 = self.0.rem_euclid(100);
-        (self.0 == 0).into()
+
+        ((self.0 == 0).into(), zero_count as usize)
     }
 
-    fn turn_right(&mut self, d: i16) -> AtZero {
+    fn turn_right(&mut self, d: i16) -> (AtZero, usize) {
         self.0 += d;
+        let zero_count = self.0 / 100;
         self.0 = self.0.rem_euclid(100);
-        (self.0 == 0).into()
+
+        ((self.0 == 0).into(), zero_count as usize)
     }
 }
 
@@ -74,11 +85,12 @@ fn main(input: &str) -> (usize, usize) {
     let rotations = input.lines().map(|l| Rotation::from(l)).collect_vec();
 
     let mut dial = Dial::new();
-    let password = rotations
+    let turns = rotations
         .iter()
         .map(|r| dial.turn(r.direction, r.clicks))
-        .filter(|z| z == &AtZero::Yes)
-        .count();
+        .collect_vec();
+    let p1 = turns.iter().filter(|t| t.0 == AtZero::Yes).count();
+    let p2 = turns.iter().map(|t| t.1).sum();
 
-    (password, 0)
+    (p1, p2)
 }
