a68b100128faa40ab978610fc15f3e9afaa32188
Max Crone <code@maxcrone.org>
2025-12-04 11:53:50 +0100

    Add day 4 part 2

--- 2025/src/bin/04.rs
+++ 2025/src/bin/04.rs
@@ -1,10 +1,10 @@
 use itertools::Itertools;
 
-fn get(grid: &[&[u8]], r: usize, c: usize) -> u8 {
+fn get(grid: &[Vec<u8>], r: usize, c: usize) -> u8 {
     *grid.get(r).and_then(|row| row.get(c)).unwrap_or(&b'.')
 }
 
-fn count_adjacent_paper(grid: &[&[u8]], p: (usize, usize)) -> usize {
+fn count_adjacent_paper(grid: &[Vec<u8>], p: (usize, usize)) -> usize {
     (-1..=1)
         .cartesian_product(-1..=1)
         .filter(|&(r, c)| (r, c) != (0, 0))
@@ -18,16 +18,39 @@ fn count_adjacent_paper(grid: &[&[u8]], p: (usize, usize)) -> usize {
         .count()
 }
 
-#[aoc::main(04)]
-fn main(input: &str) -> (usize, usize) {
-    let grid = input.lines().map(|l| l.as_bytes()).collect_vec();
-
-    let p1 = (0..grid.len())
+fn get_remove_ready_paper(grid: &[Vec<u8>]) -> Option<Vec<(usize, usize)>> {
+    let candidates = (0..grid.len())
         .cartesian_product(0..grid[0].len())
         .filter(|&(r, c)| grid[r][c] == b'@')
-        .map(|p| count_adjacent_paper(&grid, p))
-        .filter(|&n| n < 4)
-        .count();
+        .filter(|&p| count_adjacent_paper(grid, p) < 4)
+        .collect_vec();
 
-    (p1, 0)
+    match candidates.len() {
+        0 => None,
+        _ => Some(candidates),
+    }
+}
+
+fn remove_max_paper(mut grid: Vec<Vec<u8>>) -> usize {
+    let mut total_removed = 0;
+
+    while let Some(candidates) = get_remove_ready_paper(&grid) {
+        total_removed += candidates.len();
+        for (r, c) in candidates {
+            grid[r][c] = b'.';
+        }
+    }
+
+    total_removed
+}
+
+#[aoc::main(04)]
+fn main(input: &str) -> (usize, usize) {
+    let grid = input.lines().map(|l| l.bytes().collect_vec()).collect_vec();
+
+    let p1 = get_remove_ready_paper(&grid).unwrap().len();
+
+    let p2 = remove_max_paper(grid);
+
+    (p1, p2)
 }
