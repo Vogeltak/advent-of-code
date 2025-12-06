d38a62ced9c93f45df8db67cd379896745cc4e30
Max Crone <code@maxcrone.org>
2025-12-06 12:30:59 +0100

    Add day 6 part 2

--- 2025/src/bin/06.rs
+++ 2025/src/bin/06.rs
@@ -23,5 +23,39 @@ fn main(input: &str) -> (usize, usize) {
         })
         .sum();
 
-    (p1, 0)
+    let mut ceph_worksheet = input.lines().collect_vec();
+    let _operators = ceph_worksheet.pop();
+
+    let mut ceph_numbers = vec![vec![]];
+    for c in 0..ceph_worksheet[0].len() {
+        let n = (0..ceph_worksheet.len())
+            .map(|r| ceph_worksheet[r].as_bytes()[c] as char)
+            .filter(|ch| ch.is_ascii_digit())
+            .collect::<String>();
+
+        if n.is_empty() {
+            ceph_numbers.push(vec![]);
+        } else {
+            ceph_numbers
+                .last_mut()
+                .unwrap()
+                .push(n.parse::<usize>().unwrap());
+        }
+    }
+
+    let p2 = (0..ceph_numbers.len())
+        .map(|c| {
+            let op = operators[c];
+            (0..ceph_numbers[c].len())
+                .map(|r| ceph_numbers[c][r])
+                .reduce(|acc, n| match op {
+                    "+" => acc + n,
+                    "*" => acc * n,
+                    _ => unreachable!(),
+                })
+                .unwrap()
+        })
+        .sum();
+
+    (p1, p2)
 }
